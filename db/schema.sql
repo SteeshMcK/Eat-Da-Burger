7dcea9cc84aaeff5c585ff5c11aa7bc8762dba7e
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-07-18 21:44:36 -0700

    add .env

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE `burger` (
   `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
   `burger_name` VARCHAR( 255) NOT NULL,
   `devoured` BOOLEAN,
+  `createdAt` TIMESTAMP NOT NULL,
 
   PRIMARY KEY ( `id` ) 
 );
