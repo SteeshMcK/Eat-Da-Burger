19a2de7f0295d8d6e38932215ed823ac9703972d
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-07-29 12:05:50 -0700

    add database logins to controller and css styles to handlebars

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if(true) {
   connection = mysql.createConnection('mysql://eiwh5dyfjicshby9:q0kjknm7mul14avp@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k1huy7uprb3832k0');
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "BBto2941",
-  database: 'burgers_db'
+  host: "o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "eiwh5dyfjicshby9",
+  password: "q0kjknm7mul14avp",
+  database: 'k1huy7uprb3832k0'
 });
 };
 
