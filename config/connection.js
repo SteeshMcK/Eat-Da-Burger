9e19bbe946b09e2ed9f87395c4eddadef3cefed3
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-07-29 12:35:00 -0700

    add JAWSDB to connection.js to enable deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ var mysql = require('mysql');
 
 var connection;
 
-if(true) {
-  connection = mysql.createConnection('mysql://eiwh5dyfjicshby9:q0kjknm7mul14avp@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k1huy7uprb3832k0');
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "eiwh5dyfjicshby9",
-  password: "q0kjknm7mul14avp",
+  host: "localhost",
+  user: "root",
+  password: "BBto2941",
   database: 'k1huy7uprb3832k0'
 });
 };
